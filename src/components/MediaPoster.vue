a17926910e7f55841c6903f818a49ebd605a546a
jfarago <jared.farago@gmail.com>
2022-06-03 19:03:12 -0400

    fix(poster): add tv badge to poster

--- src/components/MediaPoster.vue
+++ src/components/MediaPoster.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="poster">
     <img :src='this.posterURL' alt='Movie Poster'/>
+    <div class="tv-badge" v-if="this.mediaType === 'tv'">TV</div>
   </div>
 </template>
 
@@ -9,13 +10,29 @@ export default {
   name: 'MediaPoster',
   props: {
     posterURL: String,
+    mediaType: String,
   }
 };
 </script>
 
 <style scoped lang="scss">
+.poster {
+  position: relative
+}
 
 img {
   width: 100%;
+}
+
+.tv-badge {
+  position: absolute;
+  bottom: 8px;
+  right: 8px;
+  background-color:black;
+  border-radius: 4px;
+  color: white;
+  opacity: 70%;
+  padding: 8px;
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="layout">
     <div class="media-container" v-for="item in this.media" :key="item.id">
-    <MediaPoster :posterURL="item.posterURL"/>
+    <MediaPoster
+      :posterURL="item.posterURL"
+      :mediaType="item.mediaType"
+    />
     </div>
   </div>
 </template>
@@ -30,11 +33,10 @@ export default {
 <style scoped lang="scss">
 .layout {
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(7, 1fr);
   grid-template-rows: 1fr;
   grid-column-gap: 12px;
   grid-row-gap: 12px;
-}
-.media-container {
+  padding: 0 12px;
 }
 </style>
