20acf4480a43f599db9fc805de86437ba13e4d8b
jfarago <jared.farago@gmail.com>
2022-06-03 17:00:23 -0400

    fix(layout): add app header and grid layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+    <h1>Vueflix</h1>
+    <nav>
+      <router-link to="/home">Home</router-link>
+      <router-link to="/popular">Popular</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      test: 1
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+.header {
+  height: 65px;
+}
+h1 {
+  margin: 12px;
+  color: red;
+  float: left;
+}
+
+a {
+  margin-left: 24px;
+  line-height: 55px;
+  font-weight: bold;
+  color: lightgray;
+  text-decoration: none;
+
+  &.router-link-exact-active {
+    color: white;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="layout">
+    <div class="media-container" v-for="item in this.media" :key="item">{{item}}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
   },
+  data() {
+    return {
+      media: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    };
+  }
 };
 </script>
+
+<style scoped lang="scss">
+.layout {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 12px;
+  grid-row-gap: 12px;
+}
+.media-container {
+  border: 1px solid white;
+  height: 200px;
+}
+</style>
